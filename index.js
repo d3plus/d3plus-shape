fcd2a1455cda8e2d3e7a2c43c980f3750bbac1de
Dave Landry <landry.dave@gmail.com>
2016-11-10 14:06:40 -0500

    implements pointDistance function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+export {default as pointDistance} from "./src/pointDistance";
+
+export {default as Image} from "./src/Image";
+
+export {default as Shape} from "./src/Shape/Shape";
 export {default as Area} from "./src/Shape/Area";
 export {default as Circle} from "./src/Shape/Circle";
-export {default as Image} from "./src/Shape/Image";
 export {default as Line} from "./src/Shape/Line";
 export {default as Path} from "./src/Shape/Path";
 export {default as Rect} from "./src/Shape/Rect";
-export {default as Shape} from "./src/Shape";
