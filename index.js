22c4138bccac5017657b7b7006a4cbd0689b6558
Dave Landry <landry.dave@gmail.com>
2016-12-02 15:27:05 -0500

    implements Bar shape

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ export {default as Image} from "./src/Image";
 
 export {default as Shape} from "./src/Shape/Shape";
 export {default as Area} from "./src/Shape/Area";
+export {default as Bar} from "./src/Shape/Bar";
 export {default as Circle} from "./src/Shape/Circle";
 export {default as Line} from "./src/Shape/Line";
 export {default as Path} from "./src/Shape/Path";
