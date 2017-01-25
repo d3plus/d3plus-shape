16f6597b00381da4a16f6648c6ff201d168e6f77
Dave Landry <landry.dave@gmail.com>
2017-01-25 12:24:42 -0500

    transcribes polyInPoly function from 1.0

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ export {default as Rect} from "./src/Shape/Rect";
 export {default as lineIntersection} from "./src/geom/lineIntersection";
 export {default as pointDistance} from "./src/geom/pointDistance";
 export {default as pointInPoly} from "./src/geom/pointInPoly";
+export {default as polyInPoly} from "./src/geom/polyInPoly";
 export {default as pointInSegmentBox} from "./src/geom/pointInSegmentBox";
 export {default as rotatePoint} from "./src/geom/rotatePoint";
 export {default as rotatePoly} from "./src/geom/rotatePoly";
---
+++ src/geom/polyInPoly.js
@@ -0,0 +1,34 @@
+import {default as pointInPoly} from "./pointInPoly";
+import {default as segmentsIntersect} from "./segmentsIntersect";
+
+/**
+    @function polyInPoly
+    @desc Checks if one polygon is inside another polygon.
+    @param {Array} polyA An Array of `[x, y]` points to be used as the inner polygon, checking if it is inside polyA.
+    @param {Array} polyB An Array of `[x, y]` points to be used as the containing polygon.
+    @returns {Boolean}
+*/
+export default function(polyA, polyB) {
+
+  let iA = -1;
+  const nA = polyA.length;
+  const nB = polyB.length;
+  let bA = polyA[nA - 1];
+
+  while (++iA < nA) {
+
+    const aA = bA;
+    bA = polyA[iA];
+
+    let iB = -1;
+    let bB = polyB[nB - 1];
+    while (++iB < nB) {
+      const aB = bB;
+      bB = polyB[iB];
+      if (segmentsIntersect(aA, bA, aB, bB)) return false;
+    }
+  }
+
+  return pointInPoly(polyA[0], polyB);
+
+}
