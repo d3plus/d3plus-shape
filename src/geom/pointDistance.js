43dd939a85ec2c41c7fedb5624c9725b592ca43a
Dave Landry <landry.dave@gmail.com>
2017-01-25 10:04:15 -0500

    simplifies pointDistance function using squaredDistance

--- src/geom/pointDistance.js
+++ src/geom/pointDistance.js
@@ -1,3 +1,5 @@
+import {default as squaredDistance} from "./squaredDistance";
+
 /**
     @function pointDistance
     @desc Calculates the pixel distance between two points.
@@ -5,8 +7,4 @@
     @param {Array} p2 The second point, which should always be an `[x, y]` formatted Array.
     @returns {Number}
 */
-export default function(p1, p2) {
-  const xx = Math.abs(p1[0] - p2[0]);
-  const yy = Math.abs(p1[1] - p2[1]);
-  return Math.sqrt(xx * xx + yy * yy);
-}
+export default (p1, p2) => Math.sqrt(squaredDistance(p1, p2));
