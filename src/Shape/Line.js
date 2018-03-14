bee1e142eecdfdb8c6c09dee876689d5cd175dc8
Nick Bond <bond.n@husky.neu.edu>
2018-03-14 14:51:50 -0400

    uses hitArea method in Shape.js to render default hitArea for Line

--- src/Shape/Line.js
+++ src/Shape/Line.js
@@ -4,7 +4,7 @@ import {interpolatePath} from "d3-interpolate-path";
 import {select} from "d3-selection";
 import * as paths from "d3-shape";
 
-import {attrize, constant, merge} from "d3plus-common";
+import {constant, merge} from "d3plus-common";
 
 import Shape from "./Shape";
 
@@ -27,6 +27,7 @@ export default class Line extends Shape {
     this._curve = "linear";
     this._defined = d => d;
     this._fill = constant("none");
+    this._hitArea = constant({"stroke-width": 10});
     this._name = "Line";
     this._path = paths.line();
     this._stroke = constant("black");
@@ -81,35 +82,23 @@ export default class Line extends Shape {
 
     const that = this;
 
-    const drawLine = (styles = {}) => {
-      this._path
-        .curve(paths[`curve${this._curve.charAt(0).toUpperCase()}${this._curve.slice(1)}`])
-        .defined(this._defined)
-        .x(this._x)
-        .y(this._y);
+    this._path
+      .curve(paths[`curve${this._curve.charAt(0).toUpperCase()}${this._curve.slice(1)}`])
+      .defined(this._defined)
+      .x(this._x)
+      .y(this._y);
 
-      this._enter.append("path")
-        .attr("transform", d => `translate(${-d.xR[0] - d.width / 2}, ${-d.yR[0] - d.height / 2})`)
-        .attr("d", d => this._path(d.values))
-        .call(this._applyStyle.bind(this))
-        .call(attrize, styles);
+    this._enter.append("path")
+      .attr("transform", d => `translate(${-d.xR[0] - d.width / 2}, ${-d.yR[0] - d.height / 2})`)
+      .attr("d", d => this._path(d.values))
+      .call(this._applyStyle.bind(this));
 
-      this._update.select("path").transition(this._transition)
-        .attr("transform", d => `translate(${-d.xR[0] - d.width / 2}, ${-d.yR[0] - d.height / 2})`)
-        .attrTween("d", function(d) {
-          return interpolatePath(select(this).attr("d"), that._path(d.values));
-        })
-        .call(this._applyStyle.bind(this))
-        .call(attrize, styles);
-    };
-
-    drawLine();
-
-    const hitAreaWidth = 10;
-
-    if (this._strokeWidth() < hitAreaWidth) {
-      drawLine({"stroke-width": hitAreaWidth, "stroke": "transparent", "fill": "transparent"});
-    }
+    this._update.select("path").transition(this._transition)
+      .attr("transform", d => `translate(${-d.xR[0] - d.width / 2}, ${-d.yR[0] - d.height / 2})`)
+      .attrTween("d", function(d) {
+        return interpolatePath(select(this).attr("d"), that._path(d.values));
+      })
+      .call(this._applyStyle.bind(this));
 
     return this;
 
